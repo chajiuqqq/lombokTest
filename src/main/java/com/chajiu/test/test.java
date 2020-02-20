1919181ac8e8b34549a9e5430004dab53bf6c8c3
chajiuqqq <chajiuqqq@outlook.com>
2020-02-20 19:02:12 +0800

    whole lombok-test

---
+++ src/main/java/com/chajiu/test/test.java
@@ -0,0 +1,13 @@
+package com.chajiu.test;
+
+import com.chajiu.pojo.User;
+
+public class test {
+    public static void main(String[] args) {
+        User user=new User();
+        user.setName("sd");
+        user.setId(12);
+        user.setAge(123);
+        System.out.println(user);
+    }
+}
